ae3c256196aa11762d1719d23d8d37a58e76ce53
Tim Beccue <timothybeccue@gmail.com>
2023-06-26 15:22:54 -0700

    Add deploy script

--- app.py
+++ app.py
@@ -190,8 +190,6 @@ def handle_disconnect():
 def handle_my_ping():
     emit('my_pong')
 
-
-#socketio.on_namespace(MyNamespace('/'))
-
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port="5001")
+    #socketio.run(app, host="0.0.0.0", port="5001")
+    socketio.run(app)
